af49bfc9ab648ef616bb6045ab490039e7bd377c
Kristin Hoffer <kristinlhoffer@gmail.com>
2016-01-10 19:03:53 -0800

    try changing carrierwave config file

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,17 +1,17 @@
 
   CarrierWave.configure do |config|
-    config.root = Rails.root.join('tmp') # adding these...
-	  config.cache_dir = 'carrierwave' # ...two lines
-
+    if Rails.env.staging? || Rails.env.production?
+	  config.storage = :fog
 	  config.fog_credentials = {
 	    :provider               => 'AWS',                        # required
 	    :aws_access_key_id      => ENV['S3_ACCESS_KEY'],                        # required
 	    :aws_secret_access_key  => ENV['S3_SECRET_KEY'],                     # required
-	    :region                 => 'en_US.UTF-8',                  # optional, defaults to 'us-east-1'
-	    :host                   => 's3.example.com',             # optional, defaults to nil
-	    :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
 	  }
-	  config.fog_directory  = ENV['S3_Bucket']                             # required
+	  config.fog_directory  = 'catherinlenore'                             # required
 	  config.fog_public     = false                                   # optional, defaults to true
 	  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+	else
+		config.storage = :file
+		config.enable_processing = Rails.env.production?
 	end
+end
